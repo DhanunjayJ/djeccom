db286155cf7ea13974417c16de319f52912aac2f
DhanunjayJ <jagudhanunjai@gmail.com>
2026-07-12 22:05:06 +0530

    Initialized Frontend and Backend

---
+++ eccom-backend/src/main/java/com/dj/eccom_backend/EccomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dj.eccom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomBackendApplication.class, args);
+	}
+
+}
---
+++ eccom-backend/src/test/java/com/dj/eccom_backend/EccomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.eccom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
